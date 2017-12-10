7cff36b91e3229c0926121414c02b9c57ec169b3
ZoDream <648383079@qq.com>
2017-12-10 14:11:32 +0800

    上传

---
+++ src/Bootstrap/RowWidget.php
@@ -0,0 +1,28 @@
+<?php
+namespace Zodream\Html\Bootstrap;
+/**
+ * Created by PhpStorm.
+ * User: zx648
+ * Date: 2016/6/3
+ * Time: 21:08
+ */
+use Zodream\Html\Widget;
+
+class RowWidget extends Widget {
+
+    protected $default = array(
+        'size' => ['md'],
+        'columns' => [
+            //'2' => ''
+        ]
+    );
+
+    protected function run() {
+        $content = null;
+        $size = $this->get('size');
+        foreach ($this->get('columns', array()) as $key => $item) {
+            $content .= Html::col($item, $key, $size);
+        }
+        return Html::row($content);
+    }
+}
