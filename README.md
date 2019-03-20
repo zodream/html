32962ec2564e9eb8eb9cf3165565fc4cfe2403bf
zodream <648383079@qq.com>
2019-03-20 16:34:01 +0800

    增加进度

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # html
 html模块组
+
+
+## Progress 使用指南
+
+### 注意：此方法不适用于并发高场景，目前不支持嵌套
+
+实例
+```php
+
+class Progress extends Zodream\Html\Progress {
+    
+    public function init() {
+        $this->data = [$this->options['a'], 2];
+        $this->setStart(0);
+    }
+
+
+    public function play($item) {
+        // $item = 1
+        // $item = 2
+    }
+}
+
+$progress = new Progess([
+    'a' => 1
+])
+$progress();
+
+```
+
+具体和web 协作显示进度
+
+```php
+
+function getProgress() {
+    if (isset($_POST['key'])) {
+        return cache($_POST['key']);
+    }
+    return new Progess([
+       'a' => 1
+   ]);
+}
+
+$progress = getProgress();
+json_encode($progress());
+
+```
+
+js 部分
+
+```js
+loopStep = function (data) {
+    if (data.key) {
+        postJson('/async', data, loopStep);
+        return;
+    }
+    // 完成
+}
+postJson('/async', {}, loopStep);
+```
+
+返回值说明
+```php
+'key' => '缓存key, 有时表示执行未完成，需要传此key继续',
+'current' => '当前执行的最后一个',
+'next' => '下一个',
+'count' => '总个数',
+'time' => '本次执行时间',
+'spent' => '总执行时间'
+```
+
+完成返回
+```php
+'count' => '总个数',
+'spent' => '总执行时间'
+```
---
+++ src/Progress.php
@@ -0,0 +1,124 @@
+<?php
+namespace Zodream\Html;
+
+use Zodream\Helpers\Str;
+use Zodream\Helpers\Time;
+
+/**
+ * 结合数据实现断点继续
+ * @package Zodream\Html
+ */
+class Progress {
+
+    protected $options = [];
+    protected $start_at = 0;
+    protected $spent = 0;
+    protected $data = [];
+    protected $index = -1;
+    protected $booted = false;
+    protected $cache_key;
+    protected $max_time = 10;
+
+    public function __construct($options = []) {
+        $this->options = $options;
+        $this->start_at = time();
+    }
+
+    /**
+     * @param int $index
+     * @return Progress
+     */
+    public function setStart(int $index) {
+        $this->index = $index - 1;
+        return $this;
+    }
+
+    public function init() {
+    }
+
+    protected function doInit() {
+        if ($this->booted) {
+            return;
+        }
+        $this->cache_key = md5(static::class.Str::random().Time::millisecond());
+        $this->spent = 0;
+        $this->init();
+        $this->booted = true;
+    }
+
+    public function current() {
+        return $this->data[$this->index];
+    }
+
+    /**
+     * @param int $index 支持指定开始个数，从0开始 -1 为不指定
+     * @return array
+     * @throws \Exception
+     */
+    public function invoke($index = -1) {
+        $this->doInit();
+        if ($index >= 0) {
+            $this->setStart($index);
+        }
+        while ($this->canDoNext()) {
+            $this->index ++;
+            if ($this->index >= count($this->data)) {
+                return $this->finish($this->current());
+            }
+            $this->play();
+        }
+        $spent = time() - $this->start_at;
+        $this->spent += $spent;
+        cache()->set($this->cache_key, $this);
+        return [
+            'key' => $this->cache_key,
+            'current' => $this->index,
+            'next' => $this->index + 1,
+            'count' => count($this->data),
+            'time' => $spent,
+            'spent' => $this->spent
+        ];
+    }
+
+    public function play($item) {
+
+    }
+
+    protected function canDoNext() {
+        if (app('request')->isCli()) {
+            return true;
+        }
+        return time() - $this->start_at < $this->max_time;
+    }
+
+    public function finish($expire = 600) {
+        $this->spent += time() - $this->start_at;
+        if (app('request')->isCli()) {
+            echo sprintf('本次耗时：%s秒', $this->spent);
+        }
+        if ($expire <= 0) {
+            cache()->delete($this->cache_key);
+            return [
+                'count' => count($this->data),
+                'spent' => $this->spent
+            ];
+        }
+        cache()->set($this->cache_key, $this, $expire);
+        return [
+            'count' => count($this->data),
+            'spent' => $this->spent
+        ];
+    }
+
+    public function __invoke($index = -1) {
+        return $this->invoke($index);
+    }
+
+    public function __sleep() {
+        return ['options', 'data', 'index', 'booted', 'cache_key', 'max_time', 'spent'];
+    }
+
+    public function __wakeup() {
+        $this->start_at = time();
+    }
+}
